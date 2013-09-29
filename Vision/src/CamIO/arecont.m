90e8855cc55e8ff525d2ed9e7827e5a5b1827a4c
Brent Townshend <bst@tc.com>
2013-09-28 17:29:59 -0700

    Increased ROI for fish

--- Vision/src/CamIO/arecont.m
+++ Vision/src/CamIO/arecont.m
@@ -1,6 +1,7 @@
 function p=arecont(id)
 [h,p]=getsubsysaddr(sprintf('CA%d',id),'reload',false);
-url=sprintf('http://%s/image?res=half&quality=21&doublescan=0&x0=1300&x1=2900&y0=700&y1=2100',h);
+%url=sprintf('http://%s/image?res=half&quality=21&doublescan=0&x0=1300&x1=2900&y0=700&y1=2100',h);
+url=sprintf('http://%s/image?res=half&quality=21&doublescan=0&x0=900&x1=3300&y0=300&y1=2500',h);
 cmd=sprintf('curl -s ''%s'' >/tmp/im.jpg', url);
 p.captstart=now;
 system(cmd);
