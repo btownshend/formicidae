9bf7a99dafbf33a2b2ed252845646a8de367e7c9
Brent Townshend <bst@tc.com>
2013-09-29 10:41:16 -0700

    Changed ROI

--- Vision/src/CamIO/arecont.m
+++ Vision/src/CamIO/arecont.m
@@ -1,7 +1,7 @@
 function p=arecont(id)
 [h,port]=getsubsysaddr(sprintf('CA%d',id),'reload',false);
 %url=sprintf('http://%s/image?res=half&quality=21&doublescan=0&x0=1300&x1=2900&y0=700&y1=2100',h);
-url=sprintf('http://%s/image?res=half&quality=21&doublescan=0&x0=600&x1=2600&y0=500&y1=2000',h);
+url=sprintf('http://%s/image?res=half&quality=21&doublescan=0&x0=800&x1=3000&y0=800&y1=2300',h);
 cmd=sprintf('curl -s ''%s'' >/tmp/im.jpg', url);
 p=struct('captstart',now);
 system(cmd);
