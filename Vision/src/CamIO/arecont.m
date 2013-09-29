46b09c2a2716343b30a7401d5e10b1b444ce78a4
Brent Townshend <bst@tc.com>
2013-09-28 17:22:38 -0700

    Reduced camera ROI

--- Vision/src/CamIO/arecont.m
+++ Vision/src/CamIO/arecont.m
@@ -1,6 +1,6 @@
 function p=arecont(id)
 [h,p]=getsubsysaddr(sprintf('CA%d',id),'reload',false);
-url=sprintf('http://%s/image?res=full&quality=21&doublescan=0&x0=0&x1=9999&y0=0&y1=9999',h);
+url=sprintf('http://%s/image?res=half&quality=21&doublescan=0&x0=1300&x1=2900&y0=700&y1=2100',h);
 cmd=sprintf('curl -s ''%s'' >/tmp/im.jpg', url);
 p.captstart=now;
 system(cmd);
