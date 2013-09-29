b50b9c3ae599fdbf4f9626bbdbe43a5595fcc8e5
Brent Townshend <bst@tc.com>
2013-09-29 00:43:45 -0700

    Adjusted ROI

--- Vision/src/CamIO/arecont.m
+++ Vision/src/CamIO/arecont.m
@@ -1,7 +1,7 @@
 function p=arecont(id)
 [h,port]=getsubsysaddr(sprintf('CA%d',id),'reload',false);
 %url=sprintf('http://%s/image?res=half&quality=21&doublescan=0&x0=1300&x1=2900&y0=700&y1=2100',h);
-url=sprintf('http://%s/image?res=half&quality=21&doublescan=0&x0=700&x1=3500&y0=100&y1=2700',h);
+url=sprintf('http://%s/image?res=half&quality=21&doublescan=0&x0=600&x1=2600&y0=500&y1=2000',h);
 cmd=sprintf('curl -s ''%s'' >/tmp/im.jpg', url);
 p=struct('captstart',now);
 system(cmd);
